816deb22ee920ec5d3b88143ee8b2ec6286490b4
vidhya978k <vanes05f@gmail.com>
2024-01-24 15:29:35 +0530

    added

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
 
 
-var popupoverlay=document.querySelector(".popup-overlay")
-var popupbox=document.querySelector(".popup-box")
-var addpopupbutton=document.getElementById("add-popup-button")
+var popupoverlay = document.querySelector(".popup-overlay")
+var popupbox = document.querySelector(".popup-box")
+var addpopupbutton = document.getElementById("add-popup-button")
 
-addpopupbutton.addEventListener("click",function(){
-    popupoverlay.style.display="block"
-    popupbox.style.display="block"
+addpopupbutton.addEventListener("click", function () {
+    popupoverlay.style.display = "block"
+    popupbox.style.display = "block"
 })
 
 
 //selecting cancel-button
 
-var cancelpopup=document.getElementById("cancel-popup")
-cancelpopup.addEventListener("click",function(event){
+var cancelpopup = document.getElementById("cancel-popup")
+cancelpopup.addEventListener("click", function (event) {
     event.preventDefault()
-    popupoverlay.style.display="none"
-    popupbox.style.display="none"
+    popupoverlay.style.display = "none"
+    popupbox.style.display = "none"
 })
 
-var container=document.querySelector(".container")
-var addbook=document.getElementById("add-book")
-var booktitleinput=document.getElementById("book-title-input")
-var bookauthorinput=document.getElementById("book-author-input")
-var bookdescriptioninput=document.getElementById("book-description-input")
+var container = document.querySelector(".container")
+var addbook = document.getElementById("add-book")
+var booktitleinput = document.getElementById("book-title-input")
+var bookauthorinput = document.getElementById("book-author-input")
+var bookdescriptioninput = document.getElementById("book-description-input")
 
-    addbook.addEventListener("click",function(event){
+addbook.addEventListener("click", function (event) {
     event.preventDefault()
-    var div=document.createElement("div")
-    div.setAttribute("class","book-container")
-    div.innerHTML=`<h2>${bookauthorinput.value}</h2>
+    var div = document.createElement("div")
+    div.setAttribute("class", "book-container")
+    div.innerHTML = `<h2>${bookauthorinput.value}</h2>
     <h5>${bookauthorinput.value}</h5>
     <p>${bookdescriptioninput.value}</p>
     <button onclick="deletebook(event)">Delete</button>`
     container.append(div)
-    popupoverlay.style.display="none"
-    popupbox.style.display="none"
+    popupoverlay.style.display = "none"
+    popupbox.style.display = "none"
 
 })
-function deletebook(event){
-   event.target.parentElement.remove()   
+function deletebook(event) {
+    event.target.parentElement.remove()
 }
 
 
